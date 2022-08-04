9c46545e2809ef01c008fc86811356f6f995d437
João <joao_jfmx@outlook.com>
2022-08-03 22:52:05 -0300

    0.6

---
+++ src/AutonomoApp.Business/Models/Validations/CategoriaValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace AutonomoApp.Business.Models.Validations;
+
+public class CategoriaValidation : AbstractValidator<Categoria>
+{
+    public CategoriaValidation()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2,20).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+    }
+}
